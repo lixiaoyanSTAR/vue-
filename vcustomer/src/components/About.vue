83279312b4aac208a3bacb21ab526cd0d9bcb219
StarLi <1104396202@qq.com>
2018-09-04 17:29:25 +0800

    vuehoutaiguanli

---
+++ vcustomer/src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about container">
+    <h3 class="page-header">
+        我们是一群温暖而又睿智的团队，我们喜欢探索新技术，开发新未来！
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about'
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
